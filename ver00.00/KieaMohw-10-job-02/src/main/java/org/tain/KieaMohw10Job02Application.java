1fc3ce2fb19efcaa7ebc5ba0686d5ec1dfa23253
grtlinux <grtlinux@gmail.com>
2021-06-11 12:54:27 +0900

    create 1st

---
+++ ver00.00/KieaMohw-10-job-02/src/main/java/org/tain/KieaMohw10Job02Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMohw10Job02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMohw10Job02Application.class, args);
+	}
+
+}
